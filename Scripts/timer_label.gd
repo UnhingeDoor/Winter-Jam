231cd8e6c8c0ccf49184a3471d0bcf3eacfb9255
hughcf <hughcfarrington@gmail.com>
2023-12-17 18:30:59 +1100

    bugfix/global-variables
    Fix style, reset global variables on scene reset

--- Scripts/timer_label.gd
+++ Scripts/timer_label.gd
@@ -2,6 +2,6 @@ extends Label
 
 @onready var level_timer: Timer = get_tree().root.get_node("Main/LevelTimer")
 
-func _process(delta):
+func _process(_delta):
 	var time_left = String.num(level_timer.time_left, 0)
 	text = "Time remaining \n%ss" % time_left
