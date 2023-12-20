a76c7ba1529c8f73fe49905ef7c7afa400fbf1c8
LimeOnACouch <powerhoues101@gmail.com>
2023-12-21 00:08:48 +1100

    Update SQLController.gd

--- Scripts/SQLController.gd
+++ Scripts/SQLController.gd
@@ -5,34 +5,26 @@ var database : SQLite
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	##Creates database file
-	database = SQLite.new()
-	database.path="res://data.db"
-	database.open_db()
+	#database = SQLite.new()
+	#database.path="res://data.db"
+	#database.open_db()
 	
 	##Defines the data table
-	var table = {
-		"id" : {"data_type":"int", "primary_key": true, "not_null": true, "auto_increment" : true},
-		"firstName" : {"data_type":"text"},
-		"lastName" : {"data_type":"text"},
-		"nameMisspelling" : {"data_type":"text"},
-		"address" : {"data_type":"text"},
-		"addMisspelling" : {"data_type":"text"},
-		"naughtNice" : {"data_type":"text"},
-		"wishList" : {"data_type":"text"}
-	} 
-	
-	database.create_table("presents", table)
-	
-	##Adds the data
-	#var data = {
-		#"firstName" : $firstName.text,
-		#"lastName" : $lastName.text,
-		#"nameMisspelling" : $nameMis.text,
-		#"address" : $address.text,
-		#"naughtNice" : $naughtNice.text
-	#}
+	#var table = {
+		#"id" : {"data_type":"int", "primary_key": true, "not_null": true, "auto_increment" : true},
+		#"firstName" : {"data_type":"text"},
+		#"lastName" : {"data_type":"text"},
+		#"nameMisspelling" : {"data_type":"text"},
+		#"address" : {"data_type":"text"},
+		#"addMisspelling" : {"data_type":"text"},
+		#"naughtNice" : {"data_type":"text"},
+		#"wishList" : {"data_type":"text"}
+	#} 
 	#
-	#database.insert_row("presents", data)
+	#database.create_table("presents", table)
+	
+	##Database has been created and data added
+	
 	
 	pass
 
