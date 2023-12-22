25d8d9df028f7b8c227f4eba16788baee8737d8b
hughcf <hughcfarrington@gmail.com>
2023-12-22 16:39:59 +1100

    Change present colours, shapes

--- Scripts/Present.gd
+++ Scripts/Present.gd
@@ -9,12 +9,18 @@ signal inspection_exited
 var is_getting_inspected: bool = false
 var is_inspected: bool = false
 
+var gift_box: Node3D
+
+@onready var gift_boxes:= [$GiftType1, $GiftType2, $GiftType3]
 @onready var player: Node3D = get_node("../Player")
 
-func _ready():
-	pass
 
-func _process(_delta):	
+func _ready():
+	# Randomise physical properties
+	gift_box = randomise_gift_shape()
+	randomise_present_colours(gift_box)
+
+func _process(_delta):
 	# Despawn the present when it's out of frame
 	if position.x > player.position.x + 10:
 		queue_free()
@@ -35,19 +41,44 @@ func initialise(spawn_pos: Vector3 , obj):
 	obj.present_accepted.connect(_on_player_present_accepted)
 	obj.present_rejected.connect(_on_player_present_rejected)
 	obj.present_recycled.connect(_on_player_present_recycled)
+	
+	
+	
 	position = spawn_pos
 	
 	## Get Nice/naughty
 	if GlobalVariables.naughtyNice[GlobalVariables.presentDataIndex] == "Nice":
-		$MeshInstance3D.mesh.material.albedo_color = Color(0, 1, 0, 1)
 		GlobalVariables.NaughtNiceFixing = "Nice"
 	if GlobalVariables.naughtyNice[GlobalVariables.presentDataIndex] == "Naughty":
-		$MeshInstance3D.mesh.material.albedo_color = Color(1, 0, 0, 1)
 		GlobalVariables.NaughtNiceFixing = "Naughty"
 	
 	GlobalVariables.presentDataIndex += 1
 	
 	GlobalVariables.updateTag = true;
+
+
+func randomise_gift_shape() -> Node3D:
+	var gift_type_idx = randi_range(0, 2) # 3 Types
+	# Only show the right one
+	for gift in gift_boxes:
+		gift.hide()
+	gift_boxes[gift_type_idx].show()
+	return gift_boxes[gift_type_idx]
+
+
+func randomise_present_colours(present) -> void:
+	var ribbon_material = StandardMaterial3D.new()
+	var box_material = StandardMaterial3D.new()
+	
+	var ribbon_hue = randf()
+	var box_hue = ribbon_hue + 0.5 if ribbon_hue < 0.5 else ribbon_hue - 0.5
+	
+	ribbon_material.albedo_color = Color.from_hsv(ribbon_hue, 1, 1)
+	box_material.albedo_color = Color.from_hsv(box_hue, 1, 1)
+	
+	present.get_child(0).mesh.surface_set_material(0, ribbon_material)
+	present.get_child(0).mesh.surface_set_material(1, box_material)
+
 
 func _on_inspection_entered():
 	GlobalVariables.inspection_in_progress = true
--- Scripts/fire_on_off.gd
+++ Scripts/fire_on_off.gd
@@ -8,7 +8,7 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	if GlobalVariables.fire:
 		show()
 	else:
