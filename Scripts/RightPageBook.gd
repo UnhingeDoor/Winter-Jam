65d89ec17161574d12cced5d4188382e0655d240
LimeOnACouch <powerhoues101@gmail.com>
2023-12-22 22:44:35 +1100

    Stuff

--- Scripts/RightPageBook.gd
+++ Scripts/RightPageBook.gd
@@ -1,4 +1,4 @@
-extends CollisionShape3D
+extends Label3D
 
 
 # Called when the node enters the scene tree for the first time.
--- Scripts/name_printout.gd
+++ Scripts/name_printout.gd
@@ -11,42 +11,42 @@ func _process(delta):
 	text = ""
 	
 	if GlobalVariables.presentDataIndex == 0:
-		text = text+"%s " % GlobalVariables.firstNames[GlobalVariables.presentDataIndex]
-		text = text+"%s:\n" % GlobalVariables.lastNames[GlobalVariables.presentDataIndex]
-		text = text+"%s\n" % GlobalVariables.address[GlobalVariables.presentDataIndex]
-		
-		text = text+"%s " % GlobalVariables.firstNames[GlobalVariables.presentDataIndex+1]
-		text = text+"%s:\n" % GlobalVariables.lastNames[GlobalVariables.presentDataIndex+1]
-		text = text+"%s\n" % GlobalVariables.address[GlobalVariables.presentDataIndex+1]
-		
 		text = text+"%s " % GlobalVariables.firstNames[GlobalVariables.presentDataIndex+2]
 		text = text+"%s:\n" % GlobalVariables.lastNames[GlobalVariables.presentDataIndex+2]
-		text = text+"%s\n" % GlobalVariables.address[GlobalVariables.presentDataIndex+2]
-		
-	elif GlobalVariables.presentDataIndex < GlobalVariables.firstNames.size() -1:
-		text = text+"%s " % GlobalVariables.firstNames[GlobalVariables.presentDataIndex-1]
-		text = text+"%s:\n" % GlobalVariables.lastNames[GlobalVariables.presentDataIndex-1]
-		text = text+"%s\n" % GlobalVariables.address[GlobalVariables.presentDataIndex-1]
-		
-		text = text+"%s " % GlobalVariables.firstNames[GlobalVariables.presentDataIndex]
-		text = text+"%s:\n" % GlobalVariables.lastNames[GlobalVariables.presentDataIndex]
-		text = text+"%s\n" % GlobalVariables.address[GlobalVariables.presentDataIndex]
+		text = text+"%s\n\n" % GlobalVariables.address[GlobalVariables.presentDataIndex+2]
 		
 		text = text+"%s " % GlobalVariables.firstNames[GlobalVariables.presentDataIndex+1]
 		text = text+"%s:\n" % GlobalVariables.lastNames[GlobalVariables.presentDataIndex+1]
-		text = text+"%s\n" % GlobalVariables.address[GlobalVariables.presentDataIndex+1]
-		
-	else:
-		text = text+"%s " % GlobalVariables.firstNames[GlobalVariables.presentDataIndex-2]
-		text = text+"%s:\n" % GlobalVariables.lastNames[GlobalVariables.presentDataIndex-2]
-		text = text+"%s\n" % GlobalVariables.address[GlobalVariables.presentDataIndex-2]
-		
-		text = text+"%s " % GlobalVariables.firstNames[GlobalVariables.presentDataIndex-1]
-		text = text+"%s:\n" % GlobalVariables.lastNames[GlobalVariables.presentDataIndex-1]
-		text = text+"%s\n" % GlobalVariables.address[GlobalVariables.presentDataIndex-1]
+		text = text+"%s\n\n" % GlobalVariables.address[GlobalVariables.presentDataIndex+1]
 		
 		text = text+"%s " % GlobalVariables.firstNames[GlobalVariables.presentDataIndex]
 		text = text+"%s:\n" % GlobalVariables.lastNames[GlobalVariables.presentDataIndex]
-		text = text+"%s\n" % GlobalVariables.address[GlobalVariables.presentDataIndex]
+		text = text+"%s\n\n" % GlobalVariables.address[GlobalVariables.presentDataIndex]
+		
+	elif GlobalVariables.presentDataIndex < GlobalVariables.firstNames.size() -1:
+		text = text+"%s " % GlobalVariables.firstNames[GlobalVariables.presentDataIndex+1]
+		text = text+"%s:\n" % GlobalVariables.lastNames[GlobalVariables.presentDataIndex+1]
+		text = text+"%s\n\n" % GlobalVariables.address[GlobalVariables.presentDataIndex+1]
+		
+		text = text+"%s " % GlobalVariables.firstNames[GlobalVariables.presentDataIndex]
+		text = text+"%s:\n" % GlobalVariables.lastNames[GlobalVariables.presentDataIndex]
+		text = text+"%s\n\n" % GlobalVariables.address[GlobalVariables.presentDataIndex]
+		
+		text = text+"%s " % GlobalVariables.firstNames[GlobalVariables.presentDataIndex-1]
+		text = text+"%s:\n" % GlobalVariables.lastNames[GlobalVariables.presentDataIndex-1]
+		text = text+"%s\n\n" % GlobalVariables.address[GlobalVariables.presentDataIndex-1]
+		
+	else:
+		text = text+"%s " % GlobalVariables.firstNames[GlobalVariables.presentDataIndex]
+		text = text+"%s:\n" % GlobalVariables.lastNames[GlobalVariables.presentDataIndex]
+		text = text+"%s\n\n" % GlobalVariables.address[GlobalVariables.presentDataIndex]
+		
+		text = text+"%s " % GlobalVariables.firstNames[GlobalVariables.presentDataIndex-1]
+		text = text+"%s:\n" % GlobalVariables.lastNames[GlobalVariables.presentDataIndex-1]
+		text = text+"%s\n\n" % GlobalVariables.address[GlobalVariables.presentDataIndex-1]
+		
+		text = text+"%s " % GlobalVariables.firstNames[GlobalVariables.presentDataIndex-2]
+		text = text+"%s:\n" % GlobalVariables.lastNames[GlobalVariables.presentDataIndex-2]
+		text = text+"%s\n\n" % GlobalVariables.address[GlobalVariables.presentDataIndex-2]
 		
 	pass
