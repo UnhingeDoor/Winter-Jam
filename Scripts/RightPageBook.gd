1b37c123ef63c2e3af204e7248a343dc760079f3
hughcf <hughcfarrington@gmail.com>
2023-12-22 23:20:04 +1100

    Make present colours work proper

--- Scripts/RightPageBook.gd
+++ Scripts/RightPageBook.gd
@@ -7,7 +7,7 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	text = "Wish Lists\n----------------------\n"
 	
 	if GlobalVariables.presentDataIndex == 0:
