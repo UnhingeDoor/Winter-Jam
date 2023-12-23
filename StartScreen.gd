706ebfe219e9003d4b644e67a746d2be4e93d4bc
hughcf <hughcfarrington@gmail.com>
2023-12-23 19:01:21 +1100

    Add start screen

---
+++ StartScreen.gd
@@ -0,0 +1,28 @@
+extends Control
+
+@onready var tree = get_tree()
+@onready var main_scene: PackedScene = preload("res://Scenes/main.tscn")
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$StartMain.show()
+	$StartTutorial.hide()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_start_button_pressed():
+	tree.change_scene_to_packed(main_scene)
+
+
+func _on_tutorial_button_pressed():
+	$StartMain.hide()
+	$StartTutorial.show()
+
+
+
+func _on_back_button_pressed():
+	$StartTutorial.hide()
+	$StartMain.show()
